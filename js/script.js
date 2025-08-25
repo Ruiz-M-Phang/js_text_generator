a717ed5c9feed4ce816004bd6a4a5fcce4f33b04
Ruiz-M-Phang <Ruiz.M.Phang@gmail.com>
2025-08-25 19:51:20 +0200

    JS DOM

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// let txt_generator = ["H", "HE", "HEL", "HELL", "HELLO", "HELLO ", "HELLO W",
+//      "HELLO WO", "HELLO WOR", "HELLO WORL", "HELLO WORLD", "HELLO WORLD!"];
+
+// txt_generator.toString()
+
+let text = "HELLO WORLD!";
+
+function greeting() {
+    document.getElementById("text__script").innerHTML = text;
+}
